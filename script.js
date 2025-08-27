8f8a7b1dc9fa1386da3aeb8b15dffde5d054409b
Queriiw <sidkiyildirim005@gmail.com>
2025-08-27 22:56:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
- var swiper = new Swiper(".mySwiper", {
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    pagination: { el: ".swiper-pagination", clickable: true },
+    navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+  });
