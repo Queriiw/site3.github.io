408126986f513073cc4061f7373e82ccd9eca4bd
Queriiw <sidkiyildirim005@gmail.com>
2025-08-28 16:55:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-var swiper = new Swiper(".mySwiper", {
-  loop: true, 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 5000,  
-    disableOnInteraction: false, 
-  },
-});
+document.addEventListener("DOMContentLoaded", () => {
+  var swiper = new Swiper(".mySwiper", {
+    loop: true, 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,  
+      disableOnInteraction: false, 
+    },
+  });
 
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
 
-hamburger.addEventListener('click', () => {
+  document.querySelector('.hamburger').onclick = function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+};
+
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu');
+
+  hamburger.addEventListener('click', () => {
     menu.classList.toggle('open');
     document.body.style.overflow = menu.classList.contains('open') ? 'hidden' : 'auto';
+  });
 });
